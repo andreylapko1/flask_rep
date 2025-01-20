44eb351a094eb9bc501f13c189200fb3be5ef9d1
AndreyLapko1 <andreycgo765@gmail.com>
2025-01-20 15:49:31 +0300

    upd

--- first.py
+++ first.py
@@ -2,9 +2,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/hello/<user>')
+def main(user):
+    return f'<br><br><h2>Hello, {user}!</h2>'
+
+@app.route('/sum/<int:a>_<int:b>')
+def suma(a, b):
+    return f'{a + b}'
 @app.route('/')
-def main():
-    return '<h3> Hello World! </h3>'
+def hello():
+    return 'Hello, World!'
+
+@app.route('/mult/<int:a>')
+def multiply(a):
+    return f'{a * 2}'
+
+
+@app.route('/multdot/<float:a>')
+def multiplydot(a):
+    return f'{a * 2}'
+
+@app.route('/div/<path:a>')
+def to_path(a):
+    return f'{a[::-1]}'
+
 
 if __name__ == '__main__':
     app.run(debug=True,

25f720ce7e35f1a7fe0959aae4db5df92e0c8e02
AndreyLapko1 <andreycgo765@gmail.com>
2025-01-28 14:32:55 +0300

    update hw_3.py

--- homeworks/hw_3.py
+++ homeworks/hw_3.py
@@ -1,10 +1,10 @@
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import Column, Integer, String, Numeric, Boolean, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-engine = create_engine('sqlite:///hw_3.db', echo=True)
+engine = create_engine('sqlite:///memory', echo=True)
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
@@ -15,12 +15,14 @@ class Product(Base):
     __tablename__ = 'product'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
-    price = Column(Numeric(5,2), nullable=False),
-    in_stock = Column(Boolean, nullable=False, default=True),
-    category_id = Column(Integer, ForeignKey('category.id'), nullable=False)
+    price = Column(Numeric(5,2), nullable=False)
+    in_stock = Column(Boolean, nullable=False, default=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship('Category', backref='products')
 
 class Category(Base):
-    __tablename__ = 'category',
-    id = Column(Integer, primary_key=True, nullable=False),
-    name = Column(String(100), nullable=False),
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(100), nullable=False)
     description = Column(String(255), nullable=False)
+    products = relationship('Product', backref='category')
